29f265d4be6316b9aedadbf12ef923552ab611a1
Rommel <karlsrommel255@gmail.com>
2023-12-13 12:16:08 -0600

    PracticaDeJS0015

---
+++ index.js
@@ -0,0 +1,14 @@
+let miArray = [1, 2, 3, 4, 5];
+
+
+let miObjeto = {
+    nombre: "Juan",
+    edad: 30,
+    ciudad: "Madrid"
+   };
+
+
+   function saludo(nombre) {
+    return "Hola, " + nombre;
+   }
+   
